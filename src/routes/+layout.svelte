6ca11e5ce920fbbe764a37f485e9dbab9eeccd6b
Ronsen <ronsen@startrek.or.id>
2022-10-19 16:18:40 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
 	import "../app.css";
+
+	const refresh = async () => {
+        window.location.href = '/';
+    };
 </script>
 
-<main class="grid h-screen place-items-center p-10">
+<main class="grid h-screen place-items-center p-10" on:click={refresh}>
 <slot />
 </main>
+
+<footer class="fixed lef-0 bottom-0 w-full text-center py-2 text-xs bg-zinc-900 text-gray-500 font-semibold">
+	TAP THE PAGE TO GET ANOTHER RANDOM VERSE
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
-
-    const refresh = async () => {
-        window.location.href = '/';
-    };
 </script>
 
 <svelte:head>
@@ -12,7 +8,7 @@
 </svelte:head>
 
 <div class="verse w-full lg:w-2/3 text-center">
-    <div class="text-3xl mb-6"><button on:click={refresh}>{data.content}</button></div>
+    <div class="text-3xl mb-6">{data.content}</div>
     
     <div class="text-gray-300 text-lg">
         <span>&mdash;</span>
