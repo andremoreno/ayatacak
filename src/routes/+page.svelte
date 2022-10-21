36c46ebb8ba27c4ac2e23cb0cdcbc141aa36b80c
Ronsen <ronsen@startrek.or.id>
2022-10-21 09:31:00 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,26 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
+
+    const refresh = () => {
+        window.location.href = '/';
+    };
 </script>
 
 <svelte:head>
     <title>Ayat Acak Alkitab</title>
 </svelte:head>
 
-<div class="verse w-full lg:w-2/3 text-center p-10 pb-20">
-    <div class="text-3xl mb-6">{data.content}</div>
-    
-    <div class="text-gray-300 text-lg">
-        <span>&mdash;</span>
-        {data.verse}
+<main class="grid h-screen place-items-center" on:click={refresh} on:keypress={refresh}>
+    <div class="verse w-full lg:w-2/3 text-center p-10 pb-20">
+        <div class="text-3xl mb-6">{data.content}</div>
+        
+        <div class="text-gray-300 text-lg">
+            <span>&mdash;</span>
+            {data.verse}
+        </div>
     </div>
-</div>
+</main>
 
 <footer class="fixed left-0 bottom-0 w-full text-center p-2 text-xs bg-zinc-900 text-zinc-500 uppercase font-bold">
 	Ketuk di mana saja untuk mendapatkan ayat yang baru
--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,35 +1,30 @@
-<div class="w-full lg:w-2/3 p-10">
-    <h3 class="font-bold text-2xl border-b pb-2 mb-2">Privacy Policy</h3>
+<script>
+    import Home from "../../lib/components/home.svelte";
+</script>
+
+<svelte:head>
+    <title>Kebijakan Privasi</title>
+</svelte:head>
+
+<Home />
+
+<div class="w-full lg:w-2/3 p-10 mx-auto">
+    <h3 class="font-bold text-2xl border-b pb-2 mb-2">Kebijakan Privasi</h3>
 
     <p class="mb-3">
-        Ayat Acak Alkitab operates the https://ayatacak.vercel.app/ website,
-        which provides the SERVICE.
+        Ayat Acak Alkitab mengoperasikan situs https://ayatacak.vercel.app/,
+        yang menyediakan LAYANAN.
     </p>
 
     <p class="mb-3">
-        This page is used to inform website visitors regarding our policies with the
-        collection, use, and disclosure of Personal Information if anyone decided to
-        use our Service, the Ayat Acak Alkitab website.
-    </p>
-
-    <p class="mb-3">
-        If you choose to use our Service, then you agree to the collection and use
-        of information in relation with this policy. The Personal Information that
-        we collect are used for providing and improving the Service. We will not use
-        or share your information with anyone except as described in this Privacy
-        Policy.
-    </p>
-
-    <p class="mb-3">
-        The terms used in this Privacy Policy have the same meanings as in our Terms
-        and Conditions, which is accessible at https://ayatacak.vercel.app/, unless
-        otherwise defined in this Privacy Policy.
+        Laman ini memberitahukan bahwa kami tidak melakukan pengumpulan informasi 
+        pribadi dalam bentuk apapun di Layanan ini.
     </p>
     
-    <h4 class="text-lg mb-3">Contact Us</h4>
+    <h4 class="text-lg mt-3 mb-3">Kontak</h4>
 
     <p class="mb-3">
-        If you have any questions or suggestions about our Privacy Policy, do not
-        hesitate to contact us.
+        Jika Anda memiliki pertanyaan atau saran tentang Kebijakan Privasi kami, 
+        jangan ragu untuk menghubungi kami.
     </p>
 </div>
