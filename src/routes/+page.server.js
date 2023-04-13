fec81dab29d2a97a871ded060731f5658d89842b
Ronsen <ronsen@startrek.or.id>
2023-04-13 14:49:06 +0700

    update

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,14 @@
 import { alkitab, getRandomVerse, translateVerse } from '$lib/services';
 
 /** @type {import('./$types').PageLoad} */
-export const load = async ({ url }) => {
+export const load = async ({ url, cookies }) => {
+    const info = (cookies.get('info') ?? 'true') == 'true';
+
     url.searchParams.get('ref');
     let random = Math.floor(Math.random() * 10000 + 1);
 
     let translatedVerse = await translateVerse(await getRandomVerse());
     let verse = await alkitab(translatedVerse);
 
-    return { verse, random };
+    return { verse, random, info };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
     /** @type {import('./$types').PageData} */
     export let data;
 
+    let info = data.info;
+
     const reload = () => {
         location.href = '/';
     };
@@ -20,6 +22,12 @@
             anchor.remove();
         });
     }
+
+    const updateInfo = () => {
+        info = !info;
+
+        document.cookie = `info=${info};max-age=2592000;path="/"`
+    };
 </script>
 
 <svelte:head>
@@ -38,7 +46,7 @@
 </main>
 
 <div class="fixed top-4 right-12">
-    <a href="/" on:click|preventDefault={download}>
+    <a href={"#"} on:click|preventDefault={download}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
             <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
@@ -46,13 +54,17 @@
     </a>
 </div>
 
+{#if info}
 <div class="fixed bottom-0 w-full p-3 text-sm">
-    <div class="p-3 bg-zinc-900 text-zinc-500 rounded-md shadow">
-        <div class="inline-flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-            </svg>
+    <div class="p-3 bg-zinc-900 text-zinc-400 rounded-md shadow">
+        <div class="flex justify-between items-center gap-3">
             Ketuk atau klik di mana saja untuk menampilkan ayat yang baru.
+            <a href={"#"} on:click|preventDefault={updateInfo}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                </svg>
+            </a>
         </div>
     </div>
 </div>
+{/if}
